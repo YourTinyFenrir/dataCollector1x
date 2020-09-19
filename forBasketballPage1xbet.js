88296d241f4048f650bc91e940fffacffe731379
Денис Львов <CSKANORD@gmail.com>
2020-09-19 11:28:22 +0300

    Debugged saving data to file

--- forBasketballPage1xbet.js
+++ forBasketballPage1xbet.js
@@ -4,8 +4,7 @@
 // @author YourTinyFenrir
 // @license MIT
 // @version 0.1
-// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js
-// @include https://bet-1x*.com/ru/line/Basketball/*
+// @include https://1x-bet*.com/ru/line/Basketball/*
 // ==/UserScript==
 
 function isReady(expressionFunction, completionFunction) {
@@ -16,9 +15,7 @@ function isReady(expressionFunction, completionFunction) {
        setTimeout(() => { isReady(expressionFunction, completionFunction); }, 50);
 }
 
-function collectData(listOfMatches) {
-
-    //console.log("Subgame " + curSubgame + "/" + numOfSubgame);
+function collectData() {
 
     isReady(() => document.getElementsByClassName("bet_group")[0] != undefined, () => {
         isReady(() => document.getElementsByClassName("subgames") != undefined, () => {
@@ -26,11 +23,8 @@ function collectData(listOfMatches) {
             let curSubgame = parseInt(localStorage.getItem("currentSubgame"), 10);
             let numOfSubgame = parseInt(localStorage.getItem("numOfSubgame"), 10);
 
-            let match = {
-                date: localStorage.getItem("date"),
-                name: localStorage.getItem("name"),
-                bets: new Array(),
-            };
+            let match = JSON.parse(localStorage.getItem("match"));
+            let listOfMatches = JSON.parse(localStorage.getItem("listOfMatches"));
 
             let str = 0;
             let re = 0;
@@ -39,6 +33,12 @@ function collectData(listOfMatches) {
 
             if (curSubgame == -1) {
 
+                match = {
+                    date: localStorage.getItem("date"),
+                    name: localStorage.getItem("name"),
+                    bets: new Array(),
+                };
+
                 if (document.getElementsByClassName("subgames").length > 0) {
                     isReady(() => document.getElementsByClassName("subgames")[0].children != undefined, () => {
                         numOfSubgame = document.getElementsByClassName("subgames")[0].children.length;
@@ -46,7 +46,7 @@ function collectData(listOfMatches) {
                     });
                 }
 
-                alert("Subgame " + curSubgame + "/" + numOfSubgame);
+                console.log("Subgame " + curSubgame + "/" + numOfSubgame);
 
                 let numOfBets = document.getElementsByClassName("bet_group").length;
                 for (let i = 0; i < numOfBets; ++i) {
@@ -66,6 +66,7 @@ function collectData(listOfMatches) {
                 if (document.getElementsByClassName("subgames").length > 0) {
                     curSubgame++;
                     localStorage.setItem("currentSubgame", curSubgame);
+                    localStorage.setItem("match", JSON.stringify(match));
                     str = document.getElementsByClassName("subgames")[0].children[curSubgame].children[1].href;
                     openWindow = window.open(str);
                     isReady(() => openWindow.closed, () => { window.close(); } );
@@ -74,11 +75,15 @@ function collectData(listOfMatches) {
                     localStorage.setItem("state", 1);
                     localStorage.setItem("currentSubgame", -1);
                     listOfMatches.push(match);
+                    localStorage.setItem("match", 0);
+                    localStorage.setItem("state", 1);
                     window.close();
                 }
             }
             else {
-                alert("Subgame " + curSubgame + "/" + numOfSubgame);
+
+                console.log("Subgame " + curSubgame + "/" + numOfSubgame);
+
                 isReady(() => document.getElementsByClassName("subgames")[0].children[curSubgame] != undefined, () => {
                     str = document.getElementsByClassName("subgames")[0].children[curSubgame].children[1].innerText;
                     re = /.-я Четверть/;
@@ -88,9 +93,9 @@ function collectData(listOfMatches) {
                         for (let i = 0; i < numOfBets; ++i) {
                              isReady(() => document.getElementsByClassName("bet_group")[i].children != undefined, () => {
                                  str = document.getElementsByClassName("bet_group")[i].children[0].innerText;
-                                 re = /Индивидуальный тотал .-го. с ОТ/;
+                                 re = /Индивидуальный тотал .-го.*/;
                                  found = str.match(re);
-                                 let re2 = /1Х2. .-я Четверть/;
+                                 let re2 = /1X2.*/;
                                  let found2 = str.match(re2);
                                  if (found != null || found2 != null) {
                                      let bet = {
@@ -105,6 +110,7 @@ function collectData(listOfMatches) {
                     if (curSubgame < numOfSubgame - 1 && curSubgame < 3) {
                         curSubgame++;
                         localStorage.setItem("currentSubgame", curSubgame);
+                        localStorage.setItem("match", JSON.stringify(match));
                         str = document.getElementsByClassName("subgames")[0].children[curSubgame].children[1].href;
                         openWindow = window.open(str);
                         isReady(() => openWindow.closed, () => { window.close(); } );
@@ -112,7 +118,11 @@ function collectData(listOfMatches) {
                     else {
                         localStorage.setItem("state", 1);
                         localStorage.setItem("currentSubgame", -1);
+                        if (listOfMatches == "0")
+                            listOfMatches = new Array();
                         listOfMatches.push(match);
+                        localStorage.setItem("match", 0);
+                        localStorage.setItem("listOfMatches", JSON.stringify(listOfMatches));
                         window.close();
                     }
                 });
@@ -126,8 +136,8 @@ function openMatchWindow() {
     isReady(() => document.getElementsByClassName("event_menu")[0] != undefined, () => {
         let numOfMatches = document.getElementsByClassName("event_menu")[0].children.length;
         let curMatch = parseInt(localStorage.getItem("currentMatch"), 10);
+
         console.log("Match " + curMatch + "/" + numOfMatches);
-        alert("Match " + curMatch + "/" + numOfMatches);
 
         if (curMatch < numOfMatches) {
             let strWindow = document.getElementsByClassName("event_menu")[0].children[curMatch].children[0].href;
@@ -150,13 +160,13 @@ function openMatchWindow() {
 
 }
 
-function openLeagueWindow(listOfMatches) {
+function openLeagueWindow() {
 
     isReady(() => document.getElementsByClassName("link link--labled")[0] != undefined, () => {
         let numOfLeagues = document.getElementsByClassName("link link--labled").length;
         let curLeague = parseInt(localStorage.getItem("currentLeague"), 10);
+
         console.log("League " + curLeague + "/" + numOfLeagues);
-        alert("League " + curLeague + "/" + numOfLeagues);
 
         if (curLeague < numOfLeagues) {
             let str = document.getElementsByClassName("link link--labled")[curLeague].href;
@@ -167,8 +177,12 @@ function openLeagueWindow(listOfMatches) {
             isReady(() => openWindow.closed, () => { openLeagueWindow(curLeague, numOfLeagues); } );
         }
         else {
-            localStorage.setItem("json", JSON.stringify(listOfMatches));
-            console.log("All data has been successfully collected");
+            let listOfMatches = JSON.parse(localStorage.getItem("listOfMatches"));
+            let a = document.createElement("a");
+            let file = new Blob([JSON.stringify(listOfMatches, null, "\t")], { type: "text/plain" });
+            a.href = URL.createObjectURL(file);
+            a.download = "1x.txt";
+            a.click();
         }
     });
 
@@ -178,11 +192,11 @@ let st = localStorage.getItem("state");
 let listOfMatches = new Array();
 
 if (st == '0') {
-    openLeagueWindow(listOfMatches);
+    openLeagueWindow();
 }
 else if (st == '1') {
     openMatchWindow();
 }
 else {
-    collectData(listOfMatches);
+    collectData();
 }
